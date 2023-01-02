8e64c65a6ac71840c90be00315a69f578ee7e77b
NikSolo <>
2023-01-02 17:23:37 +0300

    Start new game script

--- Assets/CodeBase/Logic/StartNewGame.cs
+++ Assets/CodeBase/Logic/StartNewGame.cs
@@ -1,5 +1,7 @@
-﻿using CodeBase.Infrastructure.Services;
+﻿using CodeBase.Data;
+using CodeBase.Infrastructure.Services;
 using CodeBase.Infrastructure.States;
+using CodeBase.Services.PersistentProgress;
 using CodeBase.Services.SaveLoad;
 using CodeBase.UI.UIInventory.Interfaces;
 using UnityEngine;
@@ -14,6 +16,7 @@ namespace CodeBase.Logic
             PlayerPrefs.Save();
             AllServices.Container.Single<IUIItemInventory>().Clear();
             AllServices.Container.Single<ISaveLoadService>().CleanUp();
+            AllServices.Container.Single<IPersistentProgressService>().Progress = new PlayerProgress(Constants.InitialLevel);
             AllServices.Container.Single<IGameStateMachine>().Enter<BootstrapState>();
         }
     }
