dba7eb527aa0721b73ad71213421167e4dbf850e
NikSolo <>
2023-01-02 14:31:09 +0300

    added follow borders for camera follow

--- Assets/CodeBase/CameraLogic/CameraFollow.cs
+++ Assets/CodeBase/CameraLogic/CameraFollow.cs
@@ -1,8 +1,7 @@
-﻿using CodeBase.Infrastructure.Services;
-using CodeBase.Services.Camera;
+﻿using CodeBase.Services.Camera;
 using CodeBase.Services.Update;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.Events;
 
 namespace CodeBase.CameraLogic
 {
@@ -10,20 +9,27 @@ namespace CodeBase.CameraLogic
     {
         [SerializeField] private Vector3 _offset;
         [SerializeField] private float _smoothSpeed = 0.125f;
-        [SerializeField] private bool enableBounds;
-        [SerializeField] private float _minBoundX;
-        [SerializeField] private float _maxBoundX;
+        [SerializeField] private bool _enableBorders;
+        [SerializeField] private float _xLeftBorder;
+        [SerializeField] private float _xRightBorder;
 
         private Transform _body;
         private Vector3 _desiredPosition;
+        private Vector3 _previousPosition;
+
+        private float _endPosition;
+
         private IUpdateService _updateService;
         private ICameraRaycast _cameraRayCast;
 
-        public void Construct(Transform body, ICameraRaycast cameraRayCast,IUpdateService updateService)
+        public UnityEvent OnCameraMoving;
+        public UnityEvent OnCameraNotMoving;
+
+        public void Construct(Transform body, ICameraRaycast cameraRayCast, IUpdateService updateService)
         {
             _body = body;
             _cameraRayCast = cameraRayCast;
-            
+
             _updateService = updateService;
             _updateService.Register(this);
         }
@@ -38,7 +44,7 @@ namespace CodeBase.CameraLogic
             if (_body)
             {
                 Vector3 lastHitPoint = _cameraRayCast.GetLastHitPoint();
-              
+
                 if (lastHitPoint.x > _body.transform.position.x)
                 {
                     _desiredPosition = _body.transform.position + _offset;
@@ -48,33 +54,29 @@ namespace CodeBase.CameraLogic
                     _desiredPosition = _body.transform.position - _offset;
                 }
 
+
                 _desiredPosition.y = transform.position.y;
                 _desiredPosition.z = transform.position.z;
-                
-                if (enableBounds)
-                {
-                    float newPos;
-                    if (transform.position.x >= _maxBoundX)
-                    {
-                        newPos = transform.position.x + (_desiredPosition.x * _smoothSpeed);
-                        if (newPos <= _minBoundX)
-                        {
-                            return;
-                        }
-                    }
 
-                    if (transform.position.x <= _minBoundX)
-                    {
-                        newPos = transform.position.x - (_desiredPosition.x * _smoothSpeed);
-                        if (newPos <= _minBoundX)
-                        {
-                            return;
-                        }
-                    }
-                }
-                
                 Vector3 smoothedPosition = Vector3.Lerp(transform.position, _desiredPosition, _smoothSpeed);
                 transform.position = smoothedPosition;
+
+                if (transform.position.x == _previousPosition.x)
+                {
+                    OnCameraNotMoving?.Invoke();
+                }
+                else
+                {
+                    OnCameraMoving?.Invoke();
+                }
+
+                if (_enableBorders)
+                    if (transform.position.x < _xLeftBorder || transform.position.x > _xRightBorder)
+                    {
+                        transform.position = new Vector3(_previousPosition.x,transform.position.y,transform.position.z);
+                    }
+
+                _previousPosition = transform.position;
             }
         }
     }
